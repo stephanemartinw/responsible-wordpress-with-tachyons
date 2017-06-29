e8048bcfda6b3d06be88804ff30930e72971f54a
stephanemartinw <stephanemartinw@gmail.com>
2017-06-29 13:26:04 +0200

    Fix minor translations issues

--- plugins/wistiti/templates/contact-form.php
+++ plugins/wistiti/templates/contact-form.php
@@ -11,5 +11,5 @@
   <p><?php _e('Your Message', 'wistiti');?>*<br/>
     <textarea class="f5 pa1 w-75 w-50-ns" rows="10" cols="35" name="cf-message"><?php ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' );?></textarea>
   </p>
-  <p><input class="f5 bg-<?php echo get_theme_mod( 'smew_colors_brand', 'blue' ); ?> white bw0 pa3" type="submit" name="cf-submitted" value="<?php _e('Send', 'smew-simple-elements');?>"></p>
+  <p><input class="f5 bg-<?php echo get_theme_mod( 'smew_colors_brand', 'blue' ); ?> white bw0 pa3" type="submit" name="cf-submitted" value="<?php _e('Send', 'wistiti');?>"></p>
 </form>
