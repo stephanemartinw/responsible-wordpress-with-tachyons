e91237defaef2e79ae87ef48a3bb26698f68822d
stephanemartinw <stephanemartinw@gmail.com>
2017-07-04 15:54:54 +0200

    Fix wrapper class issue

--- plugins/wistiti/templates/partials/teammember-card.php
+++ plugins/wistiti/templates/partials/teammember-card.php
@@ -10,7 +10,7 @@
   wistiti_get_template('/partials/customizers/teammember-card-customizer.php', $atts);
 ?>
 
-<div class=<?php echo $partial_args['classes']['wrapper']?>>
+<div class="<?php echo $partial_args['classes']['wrapper'];?>">
   <?php the_post_thumbnail( 'medium_large', ['class' => $partial_args['classes']['thumbnail']]); ?>
   <h<?php echo $firstheadinghierarchy;?>><?php the_title();?></h<?php echo $firstheadinghierarchy;?>>
   <h<?php echo $secondheadinghierarchy;?>><?php echo get_post_meta( get_the_ID(), '_teammember_function', true );?></h<?php echo $secondheadinghierarchy;?>>
