cf2e51d78e78fd87fa37615b1b4df83c3e5ae7ed
stephanemartinw <stephanemartinw@gmail.com>
2017-07-19 16:54:32 +0200

    Improve no js management for navigation

--- theme/wistiti/components/navigation/navigation-main-customizer.php
+++ theme/wistiti/components/navigation/navigation-main-customizer.php
@@ -2,7 +2,8 @@
 global $navigation_args;
 $navigation_args = array(
 	'classes' => array(
-		'wrapper' => 'absolute relative-l top-100 left-0 w-100 w-auto-l z-999 dn-js-nl db-l list ma0 pa0 bg-white',
+		'navigation' => 'cb dn db-js toggler',
+		'wrapper' => 'absolute relative-l top-100 left-0 w-100 w-auto-l z-999 dn db-l list ma0 pa0 bg-white',
     'button' => 'db dn-l bn bg-transparent ttu f6 pointer pa2 bw1 b--solid b--black',
     'items' => array(
 			'level' => array(
--- theme/wistiti/components/navigation/navigation-main.php
+++ theme/wistiti/components/navigation/navigation-main.php
@@ -1,12 +1,10 @@
 <?php
 	global $navigation_args;
 	get_template_part( 'components/navigation/navigation-main', 'customizer' );
-
-	//To do : use args in this template !
 ?>
 
-<nav id="js-site-navigation" class="cb db toggler" role="navigation">
-		<button class="<?php echo $navigation_args['classes']['button']?>" aria-controls="main-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'wistiti' ); ?></button>
+<nav id="js-site-navigation" class="<?php echo $navigation_args['classes']['navigation']; ?>" role="navigation">
+		<button class="<?php echo $navigation_args['classes']['button']; ?>" aria-controls="main-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'wistiti' ); ?></button>
 		<?php
 			wp_nav_menu(array(
 			    'theme_location' => 'menu-1',
@@ -17,3 +15,21 @@
 			));
 			?>
 </nav>
+<noscript>
+	<!-- Force visibility of menu & submenu -->
+	<nav id="js-site-navigation" class="<?php echo $navigation_args['classes']['navigation']; ?> db" role="navigation">
+			<button class="<?php echo $navigation_args['classes']['button']; ?>" aria-controls="main-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'wistiti' ); ?></button>
+			<?php
+				wp_nav_menu(array(
+				    'theme_location' => 'menu-1',
+						'menu_id' => 'js-main-menu',
+						'container'=> false,
+						'items_wrap' => '<ul id="js-main-menu" class="'.$navigation_args['classes']['wrapper'].' db cmzr-navigation-menu" role="menubar">%3$s</ul>',
+				    'walker'  => new Wistiti_Walker_Main_Menu($navigation_args['classes']['items']) //use wistiti custom walker in functions.php
+				));
+				?>
+	</nav>
+</noscript>
+
+
+<?php unset($navigation_args);?>
