cba3a52b50f05f634f4aae0533a951413b3b3244
stephanemartinw <stephanemartinw@gmail.com>
2017-08-23 12:42:48 +0200

    Improve scripts conditionnal loading + page template management

--- plugins/wistiti/wistiti.php
+++ plugins/wistiti/wistiti.php
@@ -12,6 +12,13 @@ define('__ROOT__', dirname(__FILE__));
 define('__WISTITI__', 'wistiti');
 define('__MY_WISTITI__', 'my-wistiti');
 
+global $scripts;
+$scripts=array(
+  'grid' => false,
+  'button' => false,
+  'disclosure' => false
+);
+
 add_action('plugins_loaded', 'wistiti_init');
 function wistiti_init() {
     load_plugin_textdomain( 'wistiti', false, dirname(plugin_basename(__FILE__)).'/languages/' );
@@ -183,12 +190,15 @@ class wistiti_settings
      */
 
     private $options;
+    private $scripts;
 
     /**
      * Start up
      */
-    public function __construct()
+    public function __construct($scripts)
     {
+        $this->scripts = $scripts;
+
         add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
         add_action( 'admin_init', array( $this, 'page_init' ) );
     }
@@ -233,9 +243,10 @@ class wistiti_settings
      */
     public function page_init()
     {
-        register_setting('wistiti_settings_group','wistiti_include_name', array( $this, 'sanitize' ));
-        register_setting('wistiti_settings_group','wistiti_include_onpage', array( $this, 'sanitize' ));
-        register_setting('wistiti_settings_group','wistiti_tool', array( $this, 'sanitize' ));
+        register_setting('wistiti_settings_group','wistiti_scripts_name', array( $this, 'sanitize' ));
+        register_setting('wistiti_settings_group','wistiti_scripts_onpage', array( $this, 'sanitize' ));
+        register_setting('wistiti_settings_group','wistiti_modules_name', array( $this, 'sanitize' ));
+        register_setting('wistiti_settings_group','wistiti_tools', array( $this, 'sanitize' ));
 
         add_settings_section(
             'wistiti_section_general', // ID
@@ -244,11 +255,20 @@ class wistiti_settings
             'wistiti-setting-admin' // Page
         );
 
-        //Includes management
+        //Scripts management
         add_settings_field(
-            'includes', // ID
-            'Includes', // Title
-            array( $this, 'includes_callback' ), // Callback
+            'scripts', // ID
+            'Scripts', // Title
+            array( $this, 'scripts_callback' ), // Callback
+            'wistiti-setting-admin', // Page
+            'wistiti_section_general' // Section
+        );
+
+        //Modules management
+        add_settings_field(
+            'modules', // ID
+            'Modules', // Title
+            array( $this, 'modules_callback' ), // Callback
             'wistiti-setting-admin', // Page
             'wistiti_section_general' // Section
         );
@@ -270,30 +290,22 @@ class wistiti_settings
      */
     public function sanitize( $input )
     {
+
         $new_input = array();
 
-        //Includes
-        if( isset( $input['element'] ) )
-            $new_input['element'] = true;
-        if( isset( $input['element_onpage'] ) )
-            $new_input['element_onpage'] = sanitize_text_field($input['element_onpage']) ;
+        //Scripts
+        foreach ($this->scripts as $script => $script_value) {
+          if( isset( $input[$script] ) )
+              $new_input[$script] = true;
+          if( isset( $input[$script.'_onpage'] ) )
+              $new_input[$script.'_onpage'] = sanitize_text_field($input[$script.'_onpage']) ;
+        }
 
-        /*if( isset( $input['news'] ) )
-            $new_input['news'] = true ;
-        if( isset( $input['news_onpage'] ) )
-            $new_input['news_onpage'] = sanitize_text_field($input['news_onpage']) ;
-
-        if( isset( $input['team'] ) )
-            $new_input['team'] = true ;
-        if( isset( $input['team_onpage'] ) )
-            $new_input['team_onpage'] = sanitize_text_field($input['team_onpage']) ;
-        */
-
+        //Modules
         if( isset( $input['contactform'] ) )
             $new_input['contactform'] = true ;
         if( isset( $input['contactform_onpage'] ) )
             $new_input['contactform_onpage'] = sanitize_text_field($input['contactform_onpage']) ;
-
 
         //Tools
         if( isset( $input['ga'] ) )
@@ -322,30 +334,61 @@ class wistiti_settings
     /**
      * Get the settings option array and print one of its values
      */
-    public function includes_callback()
+    public function scripts_callback()
     {
-        $includes = get_option( 'wistiti_include_name' );
-        $onpages = get_option( 'wistiti_include_onpage' );
+        $includes = get_option( 'wistiti_scripts_name' );
+        $onpages = get_option( 'wistiti_scripts_onpage' );
 
-        $value = array("name" => "Post Types to include",
-        	"desc" => "Select the pages you want to include. All pages are excluded by default",
-        	"includes" => array( "element"=>"Element",
-                              //"news"=>"News",
-                              //"team"=>"Team",
-                              "contactform"=>"Contact Form")
+        $value = array("name" => "Scripts to include",
+        	"desc" => "Select the scripts you want to include. All pages are excluded by default",
+        	"scripts" => array( "button"=>"button.js",
+                              "grid"=>"grid.js",
+                              "disclosure"=>"disclosure.js")
         );
 
         $markup='<ul>'."\n";
-        foreach ($value['includes'] as $include_value => $include) {
+        foreach ($value['scripts'] as $script_value => $script) {
           $checked = " ";
-          if ($include_value) {
-            $checked = checked( 1, $includes[$include_value], false );
+          if ($script_value) {
+            $checked = checked( 1, $includes[$script_value], false );
           }
           $markup.="<li>\n";
-          $markup.='<input type="checkbox" name="wistiti_include_name['.$include_value.']" value="true" '.$checked.' />'.$include."\n";
+          $markup.='<input type="checkbox" name="wistiti_scripts_name['.$script_value.']" value="true" '.$checked.' />'.$script."\n";
 
           $markup .= '<label>'.__('on page(s): ', 'wistiti').'</label>';
-          $markup .= '<input type="text" name="wistiti_include_onpage['.$include_value.'_onpage]" value="'.$onpages[$include_value.'_onpage'].'"></input>';
+          $markup .= '<input type="text" name="wistiti_scripts_onpage['.$script_value.'_onpage]" value="'.$onpages[$script_value.'_onpage'].'"></input>';
+
+          $markup.="</li>\n";
+        }
+        $markup.= "</ul>\n";
+
+        echo $markup;
+    }
+
+    /**
+     * Get the settings option array and print one of its values
+     */
+    public function modules_callback()
+    {
+        $modules = get_option( 'wistiti_modules_name' );
+        $onpages = get_option( 'wistiti_modules_onpage' );
+
+        $value = array("name" => "Modules to include",
+          "desc" => "Select the modules you want to include. All pages are excluded by default",
+          "modules" => array( "contactform"=>"Simple Contact form")
+        );
+
+        $markup='<ul>'."\n";
+        foreach ($value['modules'] as $module_value => $module) {
+          $checked = " ";
+          if ($module_value) {
+            $checked = checked( 1, $modules[$module_value], false );
+          }
+          $markup.="<li>\n";
+          $markup.='<input type="checkbox" name="wistiti_include_name['.$module_value.']" value="true" '.$checked.' />'.$module."\n";
+
+          $markup .= '<label>'.__('on page(s): ', 'wistiti').'</label>';
+          $markup .= '<input type="text" name="wistiti_include_onpage['.$module_value.'_onpage]" value="'.$onpages[$module_value.'_onpage'].'"></input>';
 
           $markup.="</li>\n";
         }
@@ -359,7 +402,7 @@ class wistiti_settings
      */
     public function tools_callback()
     {
-      $tools = get_option( 'wistiti_tool' );
+      $tools = get_option( 'wistiti_tools' );
 
       $value = array("name" => "Tools to activate",
         "desc" => "Select the tools you want to include. All tools are excluded by default",
@@ -385,56 +428,61 @@ class wistiti_settings
 
       echo $markup;
     }
-
 }
 
 if( is_admin() )
-    $wistiti_settings_page = new wistiti_settings();
+    $wistiti_settings_page = new wistiti_settings($scripts);
 
 //General scripts
 add_action('wp_enqueue_scripts','wistiti_enqueue_scripts');
 function wistiti_enqueue_scripts() {
 
   //Set scripts configuration before...
-  $scriptsperelement=array (
+  /*$scriptsperelement=array (
     'element' => 'utils', // + disclosure grid button ?
     //'news' => 'utils grid',
     //'team' => 'utils grid',
     'contactform' => 'button'
-  );
-  $scripts=array(
-    'utils' => false,
-    'grid' => false,
-    'button' => false,
-    'disclosure' => false
-  );
-  $onpages = get_option('wistiti_include_onpage');
+  );*/
+  global $scripts;
 
-  foreach ($scriptsperelement as $element => $jss_list) {
-    $pages_list = $onpages[$element.'_onpage'];
+  $includescripts = get_option('wistiti_scripts_name');
+  $onpages = get_option('wistiti_scripts_onpage');
+
+  foreach ($scripts as $script => $script_value) {
+    $pages_list = $onpages[$script.'_onpage'];
     $pages_list = preg_replace('/\s+/', '', $pages_list);
-    if (!empty($pages_list)) {
-      $pages = explode(',', $pages_list);
+    if (isset($includescripts[$script]) && $includescripts[$script]) {
+      if (!empty($pages_list)) {
+        $pages = explode(',', $pages_list);
 
-      //Convert ids to int if necessary
-      /*foreach ($pages as $index => $page) {
-        if((string)(int)$page == $page) {
-          $pages[$index] = intval($page);
-        }
-      }*/
-
-      if (is_page($pages) || (is_front_page() && in_array('front', $pages)) ) {
-        if (!empty($jss_list)) {
-          $jss = explode(' ', $jss_list);
-          foreach ($jss as $js) {
-             $scripts[$js]= true;
+        //Convert ids to int if necessary
+        /*foreach ($pages as $index => $page) {
+          if((string)(int)$page == $page) {
+            $pages[$index] = intval($page);
           }
+        }*/
+
+        if (is_page($pages) || (is_front_page() && in_array('front', $pages)) ) {
+          $scripts[$script] = true;
+
+          /*if (!empty($jss_list)) {
+            $jss = explode(' ', $jss_list);
+            foreach ($jss as $js) {
+               $scripts[$js]= true;
+            }
+          }*/
         }
+      }
+      else {
+      //No pages list given...script is included on all pages ?
+        $scripts[$script] = true;
       }
     }
   }
 
-  if ($scripts['utils']) wp_enqueue_script( 'wistiti-utils', plugins_url( '/js/utils.js', __FILE__ ), array());
+  if ($scripts['grid'])
+    wp_enqueue_script( 'wistiti-utils', plugins_url( '/js/utils.js', __FILE__ ), array());
   if ($scripts['grid']) wp_enqueue_script( 'wistiti-grid', plugins_url( '/js/grid.js', __FILE__ ), array('wistiti-utils'));
   if ($scripts['button']) wp_enqueue_script( 'wistiti-button', plugins_url( '/js/button.js', __FILE__ ), array());
   if ($scripts['disclosure']) wp_enqueue_script( 'wistiti-disclosure', plugins_url( '/js/disclosure.js', __FILE__ ), array());
@@ -472,27 +520,28 @@ function wistiti_split_string_instrings($classes) {
 }
 
 //General requirements
-$tools = get_option('wistiti_tool');
-
+require_once(__ROOT__.'/types/element.php');
 require_once(__ROOT__.'/inc/template-tags.php');
+require_once(__ROOT__.'/shortcodes/wistiti.php');
 
+require_once(__ROOT__.'/widgets/text.php');
+
+//Tools
+$tools = get_option('wistiti_tool');
 if ($tools['seo']) require_once(__ROOT__.'/tools/seo.php');
 if ($tools['sitemap']) require_once(__ROOT__.'/tools/sitemap.php');
 if ($tools['ga'])require_once(__ROOT__.'/tools/ga.php');
 
 //Optional requirements
-$includes = get_option('wistiti_include_name');
-if ($includes['element']) require_once(__ROOT__.'/types/element.php');
+//$includes = get_option('wistiti_include_name');
+//if ($includes['element']) require_once(__ROOT__.'/types/element.php');
 //if ($includes['news']) require_once(__ROOT__.'/types/news.php');
 //if ($includes['team']) require_once(__ROOT__.'/types/team.php');
 
-if ($includes['contactform']) require_once(__ROOT__.'/modules/contact-form.php');
+//Optional modules
+$modules = get_option('wistiti_module_name');
+if ($modules['contactform']) require_once(__ROOT__.'/modules/contact-form.php');
 
-require_once(__ROOT__.'/shortcodes/wistiti.php');
-require_once(__ROOT__.'/widgets/text.php');
-
-unset($onpages);
-unset($scriptsperelement);
-unset($jss);
+//unset($scripts);
 unset($tools);
-unset($includes);
+unset($modules);
--- theme/wistiti/page.php
+++ theme/wistiti/page.php
@@ -20,13 +20,15 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'components/page/content', 'page' );
+				if (locate_template('components/page/content-' . $post->post_name . '.php')!='')
+					get_template_part( 'components/page/content', $post->post_name );
+				else
+					get_template_part( 'components/page/content', 'page' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
-
 
 			endwhile; // End of the loop.
 			?>
